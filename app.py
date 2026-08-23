099cb953a6c9bd93f5202c9ab4f281cccbba67f7
daniecoetzee23-stack <daniecoetzee23@gmail.com>
2026-08-23 18:35:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -351,5 +351,8 @@ def stop_bot():
     bot_running = False
     return "Stopped"
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
